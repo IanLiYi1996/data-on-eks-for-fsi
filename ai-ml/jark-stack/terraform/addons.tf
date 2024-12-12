d48d058217de8b7ffebe49af52a912cc0533ff8c
Yi Li <xwzx14ly@163.com>
2024-12-12 17:42:46 +0800

    modify eks config for mount EFS

--- ai-ml/jark-stack/terraform/addons.tf
+++ ai-ml/jark-stack/terraform/addons.tf
@@ -458,3 +458,98 @@ data "aws_iam_policy_document" "karpenter_controller_policy" {
     sid       = "KarpenterControllerAdditionalPolicy"
   }
 }
+
+#---------------------------------------------------------------
+# EFS Filesystem for private volumes per user
+# This will be replaced with Dynamic EFS provision using EFS CSI Driver
+#---------------------------------------------------------------
+resource "aws_efs_file_system" "efs" {
+  encrypted = true
+
+  tags = local.tags
+}
+
+#---------------------------------------------------------------
+# module.vpc.private_subnets = [AZ1_10.x, AZ2_10.x, AZ1_100.x, AZ2_100.x]
+# We use index 2 and 3 to select the subnet in AZ1 with the 100.x CIDR:
+# Create EFS mount targets for the 3rd  subnet
+resource "aws_efs_mount_target" "efs_mt_1" {
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = module.vpc.private_subnets[2]
+  security_groups = [aws_security_group.efs.id]
+}
+
+# Create EFS mount target for the 4th subnet
+resource "aws_efs_mount_target" "efs_mt_2" {
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = module.vpc.private_subnets[3]
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_security_group" "efs" {
+  name        = "${local.name}-efs"
+  description = "Allow inbound NFS traffic from private subnets of the VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow NFS 2049/tcp"
+    cidr_blocks = module.vpc.vpc_secondary_cidr_blocks
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+  }
+
+  tags = local.tags
+}
+
+#---------------------------------------
+# EFS Configuration
+#---------------------------------------
+module "efs_config" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.2"
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  helm_releases = {
+    efs = {
+      name             = "efs"
+      description      = "A Helm chart for storage configurations"
+      namespace        = "jupyterhub"
+      create_namespace = false
+      chart            = "${path.module}/helm-values/efs"
+      chart_version    = "0.0.1"
+      values = [
+        <<-EOT
+          pv:
+            name: efs-persist
+            dnsName: ${aws_efs_file_system.efs.dns_name}
+          pvc:
+            name: efs-persist
+        EOT
+      ]
+    }
+    efs-shared = {
+      name             = "efs-shared"
+      description      = "A Helm chart for shared storage configurations"
+      namespace        = "jupyterhub"
+      create_namespace = false
+      chart            = "${path.module}/helm-values/efs"
+      chart_version    = "0.0.1"
+      values = [
+        <<-EOT
+          pv:
+            name: efs-persist-shared
+            dnsName: ${aws_efs_file_system.efs.dns_name}
+          pvc:
+            name: efs-persist-shared
+        EOT
+      ]
+    }
+  }
+
+  depends_on = [kubernetes_namespace_v1.jupyterhub]
+}
