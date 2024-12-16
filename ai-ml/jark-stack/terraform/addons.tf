2d1da3b421aca2415ca8c610b9414855ef337dee
Yi Li <xwzx14ly@163.com>
2024-12-16 14:51:04 +0800

    modify eks config for raycluster

--- ai-ml/jark-stack/terraform/addons.tf
+++ ai-ml/jark-stack/terraform/addons.tf
@@ -53,6 +53,20 @@ module "ebs_csi_driver_irsa" {
   tags = local.tags
 }
 
+module "efs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.20"
+  role_name_prefix      = format("%s-%s-", local.name, "efs-csi-driver")
+  attach_efs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:efs-csi-controller-sa"]
+    }
+  }
+}
+
 #---------------------------------------------------------------
 # EKS Blueprints Addons
 #---------------------------------------------------------------
@@ -71,6 +85,9 @@ module "eks_blueprints_addons" {
   eks_addons = {
     aws-ebs-csi-driver = {
       service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+    }
+    aws-efs-csi-driver = {
+      service_account_role_arn = module.efs_csi_driver_irsa.iam_role_arn
     }
     coredns = {
       preserve = true
